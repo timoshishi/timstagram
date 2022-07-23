4f4579385ef679121ca2cf7814c5108493c6bc5c
timoshishi <timfrrst@gmail.com>
2022-07-23 10:13:44 -0400

    Add footer, auth decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import { UserProvider } from '@supabase/auth-helpers-react';
+import { supabase } from '../src/lib/initSupabase';
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -7,3 +10,11 @@ export const parameters = {
     },
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <UserProvider supabaseClient={supabase}>
+      <Story />
+    </UserProvider>
+  ),
+];
