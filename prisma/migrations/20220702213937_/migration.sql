a02f551fdda15435d09246b561f592f0549a2369
timoshishi <timfrrst@gmail.com>
2022-08-14 21:10:56 -0400

    delete migrations

--- prisma/migrations/20220702213937_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `email` to the `user` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "user" ADD COLUMN     "email" TEXT NOT NULL;
