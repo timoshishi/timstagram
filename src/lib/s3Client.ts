64123270a09c8b8d4213563cf7d9afb6d0275fce
timoshishi <timfrrst@gmail.com>
2022-09-19 23:24:00 -0400

    chore(ci): update aws env

--- src/lib/s3Client.ts
+++ src/lib/s3Client.ts
@@ -1,3 +1,10 @@
 import { S3Client } from '@aws-sdk/client-s3';
-const s3Client = new S3Client({ region: process.env.BUCKET_REGION });
+const s3Client = new S3Client({
+  region: process.env.BUCKET_REGION,
+
+  credentials: {
+    accessKeyId: process.env.A_KEY_ID!,
+    secretAccessKey: process.env.A_SECRET_KEY!,
+  },
+});
 export { s3Client };
