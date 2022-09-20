450571d76fce0f930ed4210b6f58feff63b1a672
timoshishi <timfrrst@gmail.com>
2022-09-19 23:41:32 -0400

    fix(ci): update env name

--- src/lib/s3Client.ts
+++ src/lib/s3Client.ts
@@ -1,9 +1,8 @@
 import { S3Client } from '@aws-sdk/client-s3';
 const s3Client = new S3Client({
   region: process.env.BUCKET_REGION,
-
   credentials: {
-    accessKeyId: process.env.A_KEY_ID!,
+    accessKeyId: process.env.A_ACCESS_KEY_ID!,
     secretAccessKey: process.env.A_SECRET_KEY!,
   },
 });
