40be399369449467ad2bb7e1e8c60392698fc857
timoshishi <timfrrst@gmail.com>
2022-09-21 10:21:07 -0400

    feat(ui): add skeleton feed in stories

--- src/common/layout/Feed/FeedLayout.tsx
+++ src/common/layout/Feed/FeedLayout.tsx
@@ -1,18 +1,11 @@
 import { Box, VStack, Show } from '@chakra-ui/react';
-import { PostResponse } from 'types/post.types';
-import { SkeletonPostCard } from '@common/components/PostCard/SkeletonPostCard';
 
-export const FeedLayout = ({
-  feedResponse,
-  sidebarData = null,
-}: {
-  feedResponse?: PostResponse | undefined;
-  sidebarData?: null;
-}) => {
+export const FeedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Box w='100%' h='100%' display='flex' flexDirection='row' justifyContent={'center'}>
       <Box
-        w={['100vw', '80vw', '60vw']}
+        w={['100%', '90%', '90%', '70%', '70%']}
+        ml={[0, '20%']}
         minH='100vh'
         mt='60px'
         maxW='100%'
@@ -21,29 +14,20 @@ export const FeedLayout = ({
         alignItems={'flex-start'}
         flexDir={'row'}
       >
-        {/* <HStack w={['100%', '80%']} rowGap={12} columnGap={4} pt={50} justifyContent='center' alignItems='flex-start'> */}
-        <VStack spacing={10} flexGrow={1} h='100%' rowGap={12} columnGap={4} px={[0, 4]}>
-          {!feedResponse ? (
-            <>
-              {new Array(5).fill(
-                <Box w='100%'>
-                  <SkeletonPostCard />
-                </Box>
-              )}
-            </>
-          ) : null}
+        <VStack spacing={10} flexGrow={1} h='100%' rowGap={12} columnGap={4} px={[0]} ml={[0]}>
+          {children}
         </VStack>
-        {/* </HStack> */}
         <Show above='md' ssr>
           <Box
-            minW={['0px', '250px', '300px', '350px']}
+            minW={['0px', '250px', '250px', '300px']}
             bg='orange'
             maxH='80vh'
             minH='60vh'
             position='sticky'
             top={'60px'}
-            borderRadius={10}
+            borderRadius={5}
             shadow={'md'}
+            ml={[0, '15px']}
           />
         </Show>
       </Box>
--- src/stories/Layout/Feed.stories.tsx
+++ src/stories/Layout/Feed.stories.tsx
@@ -3,49 +3,18 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { feed } from '../../mocks/feed';
 import { FeedLayout } from '../../common/layout/Feed';
 import { Container } from '@chakra-ui/react';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+import { Box } from '@chakra-ui/react';
+import { SkeletonList } from '../../common/components/PostCard/SkeletonList';
 export default {
   title: 'Layout/Feed',
   component: FeedLayout,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
 } as ComponentMeta<typeof FeedLayout>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof FeedLayout> = (...args) => <FeedLayout {...args} />;
-
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Layout',
-};
-
-Primary.argTypes = {
-  feedResponse: {
-    defaultValue: undefined,
-    control: {
-      type: 'radio',
-    },
-    options: [undefined, 'feed'],
-  },
-};
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Layout',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Layout',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Layout',
-// };
+export const FeedNoData: ComponentStory<typeof FeedLayout> = (...args) => (
+  <FeedLayout>
+    <SkeletonList length={10} />
+  </FeedLayout>
+);
