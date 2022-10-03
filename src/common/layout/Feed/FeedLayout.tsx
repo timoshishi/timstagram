718c43e51d14a661b38080177f05c937e60ae507
timoshishi <timfrrst@gmail.com>
2022-10-03 09:24:48 -0400

    test(feed): adds e2e tests on the feed

--- src/common/layout/Feed/FeedLayout.tsx
+++ src/common/layout/Feed/FeedLayout.tsx
@@ -2,7 +2,7 @@ import { Box, VStack, Show } from '@chakra-ui/react';
 
 export const FeedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <Box w='100%' h='100%' display='flex' flexDirection='row' justifyContent={'center'}>
+    <Box w='100%' h='100%' display='flex' flexDirection='row' justifyContent={'center'} data-testid='feed-layout'>
       <Box
         w={['100%', '90%', '90%', '70%', '70%']}
         ml={[0, '20%']}
@@ -13,6 +13,7 @@ export const FeedLayout = ({ children }: { children: React.ReactNode }) => {
         justifyContent={'center'}
         alignItems={'flex-start'}
         flexDir={'row'}
+        data-testid='feed-layout-main'
       >
         <VStack spacing={10} flexGrow={1} h='100%' rowGap={12} columnGap={4} px={[0]} ml={[0]}>
           {children}
@@ -28,6 +29,7 @@ export const FeedLayout = ({ children }: { children: React.ReactNode }) => {
             borderRadius={5}
             shadow={'md'}
             ml={[0, '15px']}
+            data-testid='feed-layout-sidebar'
           />
         </Show>
       </Box>
