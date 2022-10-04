5969775fe6756c375509be25bb6edb9786513657
timoshishi <timfrrst@gmail.com>
2022-10-04 19:43:06 -0400

    test: seed likes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,6 @@ import { fetcher } from 'src/lib/axios';
 import { ImageSourceSizes, PostResponse } from 'types/post';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 
 const API = '/feed/popular';
 const PAGE_SIZE = 25;
@@ -39,14 +38,6 @@ const Feed: NextPageWithLayout = () => {
   const { data, error: feedError, mutate, size, setSize, isValidating } = useSWRInfinite(getKey, fetcher);
 
   let postResponses: PostResponse[] = data ? [].concat(...data) : [];
-
-  // refresh the feed if the user signs out
-  // useEffect(() => {
-  //   mutate([], {
-  //     revalidate: true,
-  //   });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [user]);
 
   const isLoadingInitialData = !data && !error;
 
